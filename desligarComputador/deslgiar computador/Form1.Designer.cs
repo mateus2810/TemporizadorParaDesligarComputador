c830545111ecc94a42a77a9a556c365b3435661a
Mateus Amaral <mateus.almeida@dtidigital.com.br>
2022-02-07 19:36:40 -0300

    adicionando projeto desenvolvido

---
+++ desligarComputador/deslgiar computador/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿
+namespace deslgiar_computador
+{
+    partial class Temporizador
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Desligar = new System.Windows.Forms.Button();
+            this.DesativarDesligamento = new System.Windows.Forms.Button();
+            this.ListagemTempo = new System.Windows.Forms.ComboBox();
+            this.TextoTempo = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Desligar
+            // 
+            this.Desligar.Location = new System.Drawing.Point(269, 40);
+            this.Desligar.Name = "Desligar";
+            this.Desligar.Size = new System.Drawing.Size(137, 51);
+            this.Desligar.TabIndex = 0;
+            this.Desligar.Text = "Desligar Computador";
+            this.Desligar.UseVisualStyleBackColor = true;
+            this.Desligar.Click += new System.EventHandler(this.Desligar_Click);
+            // 
+            // DesativarDesligamento
+            // 
+            this.DesativarDesligamento.Location = new System.Drawing.Point(269, 108);
+            this.DesativarDesligamento.Name = "DesativarDesligamento";
+            this.DesativarDesligamento.Size = new System.Drawing.Size(137, 43);
+            this.DesativarDesligamento.TabIndex = 1;
+            this.DesativarDesligamento.Text = "Desativar Desligamento";
+            this.DesativarDesligamento.UseVisualStyleBackColor = true;
+            this.DesativarDesligamento.Click += new System.EventHandler(this.DesativarDesligamento_Click);
+            // 
+            // ListagemTempo
+            // 
+            this.ListagemTempo.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.ListagemTempo.FormattingEnabled = true;
+            this.ListagemTempo.Items.AddRange(new object[] {
+            "5",
+            "10",
+            "15",
+            "20",
+            "25",
+            "30",
+            "35",
+            "40",
+            "45",
+            "50",
+            "55",
+            "60\t"});
+            this.ListagemTempo.Location = new System.Drawing.Point(31, 80);
+            this.ListagemTempo.Name = "ListagemTempo";
+            this.ListagemTempo.Size = new System.Drawing.Size(180, 33);
+            this.ListagemTempo.TabIndex = 2;
+            this.ListagemTempo.SelectedIndexChanged += new System.EventHandler(this.ListagemTempo_SelectedIndexChanged);
+            // 
+            // TextoTempo
+            // 
+            this.TextoTempo.AutoSize = true;
+            this.TextoTempo.Location = new System.Drawing.Point(31, 40);
+            this.TextoTempo.Name = "TextoTempo";
+            this.TextoTempo.Size = new System.Drawing.Size(189, 15);
+            this.TextoTempo.TabIndex = 3;
+            this.TextoTempo.Text = "Deseja desligar em quanto tempo?";
+            this.TextoTempo.Click += new System.EventHandler(this.TextoTempo_Click);
+            // 
+            // Temporizador
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(467, 217);
+            this.Controls.Add(this.ListagemTempo);
+            this.Controls.Add(this.TextoTempo);
+            this.Controls.Add(this.DesativarDesligamento);
+            this.Controls.Add(this.Desligar);
+            this.Name = "Temporizador";
+            this.Text = "Temporizador Computador";
+            this.Load += new System.EventHandler(this.Temporizador_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Desligar;
+        private System.Windows.Forms.Button DesativarDesligamento;
+        private System.Windows.Forms.ComboBox ListagemTempo;
+        private System.Windows.Forms.Label TextoTempo;
+    }
+}
+
---
+++ desligarComputador/deslgiar computador/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace deslgiar_computador
+{
+    public partial class Temporizador : Form
+    {
+        public Temporizador()
+        {
+            InitializeComponent();
+        }
+
+        private void Desligar_Click(object sender, EventArgs e)
+        {
+            SystemSounds.Beep.Play();
+
+            if (ListagemTempo.Text == "")
+            {
+                MessageBox.Show("Selecione a quantidade de tempo que deseja");
+                return;
+            }
+
+            var tempoSelecionado = Convert.ToInt16(ListagemTempo.Text);
+            var converterMinutos = tempoSelecionado * 60;
+
+            MessageBox.Show("Seu computador irá desligar em "+ tempoSelecionado + " minutos");
+            string executar = @"/C " + "shutdown -s -t "+ converterMinutos;
+            System.Diagnostics.Process.Start("CMD.exe", executar).WaitForExit();
+        }
+
+        private void DesativarDesligamento_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Temporizador de desligamento desativado");
+            string executar = @"/C " + "shutdown -a";
+            System.Diagnostics.Process.Start("CMD.exe", executar).WaitForExit();
+        }
+
+        private void Temporizador_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void ListagemTempo_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TextoTempo_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
